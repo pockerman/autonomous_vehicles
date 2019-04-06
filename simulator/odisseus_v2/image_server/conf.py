95f83d6ea3563e04d98b34bfbfa4d03f1452c568
Alexandros Giavaras <a.giavaras@gmail.com>
2019-04-06 16:57:57 +0100

    Changes to adapt to Raspberry Pi

--- simulator/odisseus_v2/image_server/conf.py
+++ simulator/odisseus_v2/image_server/conf.py
@@ -9,15 +9,15 @@ PORT =  5001
 HOST = '0.0.0.0'
 
 # Flag indicating if we are on Pi or simply emulating
-ON_RASP_PI:bool = False
-SCREEN_SIZE:tuple = (320, 240)
+ON_RASP_PI = True
+SCREEN_SIZE = (320, 240)
 ENCODE_PARAMS = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 INDEX_DISPLAY_TEMPLATE_NAME = 'image_server.html'
 CAMERA_SLEEP_TIME = 0.05
 NEEDED_CAMERA_ROTATION = 0.0
 
 # configuration for multiprocessing
-USE_MULTIPROCESSING:bool = False
+USE_MULTIPROCESSING = False
 QUEUE_MAX_SIZE=2
 
 display_queue = None
--- simulator/odisseus_v2/image_server/control_utils.py
+++ simulator/odisseus_v2/image_server/control_utils.py
@@ -19,13 +19,13 @@ def get_control_instruction(control_queue):
 def controlled_image_server_behavior(rotation, display_queue, control_queue):
 
     # Setup the camera
-    camera = pi_camera_stream.setup_camera(rotation=rotation)
+    camera = setup_camera(rotation=rotation)
     # allow the camera to warmup
     time.sleep(0.1)
 
     # Send frames from camera to server
-    for frame in pi_camera_stream.start_stream(camera):
-        encoded_bytes = pi_camera_stream.get_encoded_bytes_for_frame(frame)
+    for frame in start_stream(camera):
+        encoded_bytes = get_encoded_bytes_for_frame(frame)
         put_output_image(encoded_bytes, display_queue=display_queue)
 
         # Check any control instructions
